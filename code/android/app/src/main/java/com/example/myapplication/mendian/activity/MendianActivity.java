d3b407e2724485e54dcdc5dc6a1399c21c4181ce
yhocn121 <66580448+yhocn121@users.noreply.github.com>
2023-04-28 18:30:03 +0800

    zhaohaifei 20230428 up
    门店安卓已完成：
    1.首页界面优化；
    2.交易总额、日交易总额、月交易总额、客户信息、员工信息、商品设置、会员管理、会员等级、订单信息、统计报表：表头加了颜色，字体居中，调整表头宽度、搜索框移至屏幕下方、增加修改页面文字居中显示，点单面板更改了按钮和购物车的样式，订单号居中显示；
    未完成：
    1.点单页面中左侧列表样式和右侧商品样式
    2.商品详情页样式；

--- code/android/app/src/main/java/com/example/myapplication/mendian/activity/MendianActivity.java
+++ code/android/app/src/main/java/com/example/myapplication/mendian/activity/MendianActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.mendian.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -11,6 +12,10 @@ import com.example.myapplication.MyApplication;
 import com.example.myapplication.R;
 import com.example.myapplication.mendian.entity.YhMendianOrderDetail;
 import com.example.myapplication.mendian.entity.YhMendianUser;
+import com.youth.banner.Banner;
+import com.youth.banner.adapter.BannerImageAdapter;
+import com.youth.banner.holder.BannerImageHolder;
+import com.youth.banner.indicator.CircleIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +23,9 @@ import java.util.List;
 public class MendianActivity extends AppCompatActivity {
 
     private YhMendianUser yhMendianUser;
+
+    private Banner banner;
+    private List<Integer> banner_data;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,26 @@ public class MendianActivity extends AppCompatActivity {
         yhMendianUser = myApplication.getYhMendianUser();
         myApplication.setYhMendianOrderDetail(new YhMendianOrderDetail());
         myApplication.setOrderDetails(new ArrayList<>());
+
+        initData();
+        banner = findViewById(R.id.main_banner);
+
+        banner.setAdapter(new BannerImageAdapter<Integer>(banner_data) {
+
+            @Override
+            public void onBindView(BannerImageHolder holder, Integer data, int position, int size) {
+                holder.imageView.setImageResource(data);
+            }
+        });
+
+        // 开启循环轮播
+        banner.isAutoLoop(true);
+        banner.setIndicator(new CircleIndicator(this));
+        banner.setScrollBarFadeDuration(1000);
+        // 设置指示器颜色(TODO 即选中时那个小点的颜色)
+        banner.setIndicatorSelectedColor(Color.GREEN);
+        // 开始轮播
+        banner.start();
 
         LinearLayout Kehuinfo = findViewById(R.id.Kehuinfo);
         Kehuinfo.setOnClickListener(new View.OnClickListener() {
@@ -118,6 +146,13 @@ public class MendianActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void initData(){
+        banner_data = new ArrayList<>();
+        banner_data.add(R.drawable.lunbo1);
+        banner_data.add(R.drawable.lunbo2);
+        banner_data.add(R.drawable.lunbo3);
     }
 
     @Override
